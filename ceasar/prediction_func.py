178889ab9522d56d428df276416394839e208f79
Cashaqu <131269265+Cashaqu@users.noreply.github.com>
2023-11-02 16:36:55 +0300

    refactoring

--- ceasar/prediction_func.py
+++ ceasar/prediction_func.py
@@ -1,14 +1,10 @@
-#   function for predict
-from Ceasar.ceasar_algorithms import ceasar_cipher, shift, alphabet, to_torch, dict_char
-import torch
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+from ceasar.ceasar_algorithms import ceasar_cipher, shift, alphabet, to_torch, dict_char
 
 def prediction(test, model):
     test_enc = [ceasar_cipher(test, shift, alphabet)]
     test_tensor = to_torch(test_enc, 100, dict_char)
     model.eval()
-    #predict = model(test_tensor.to(device))
+    #predict = trained_model(test_tensor.to(device))
     predict = model(test_tensor)
     predict = predict.squeeze(0)
     predict_test = ''
